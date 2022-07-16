129524799ee3d2ac60f11e5ab75f95566b778f53
Daripher <saelver.serbek@gmail.com>
2022-07-16 12:13:59 +0500

    Revert "Added loot leveling system"
    This reverts commit 312e30f765cf102d46ebc20c6516392ba83dda50.

--- src/main/java/daripher/autoleveling/config/Config.java
+++ src/main/java/daripher/autoleveling/config/Config.java
@@ -14,26 +14,13 @@ public class Config
 	
 	public static class Common
 	{
-		public final ConfigValue<Boolean> showLevel;
-		public final ConfigValue<Double> levelBonus;
 		public final ConfigValue<Double> movementSpeedBonus;
 		public final ConfigValue<Double> flyingSpeedBonus;
 		public final ConfigValue<Double> attackDamageBonus;
 		public final ConfigValue<Double> armorBonus;
 		public final ConfigValue<Double> healthBonus;
-		
-		public final ConfigValue<Boolean> lootDropEnabled;
-		public final ConfigValue<Double> lootDropChance;
-		public final ConfigValue<Double> lootDropChancePerLevel;
-		public final ConfigValue<Double> lootDropChancePerLuck;
-		public final ConfigValue<Double> uncommonDropChance;
-		public final ConfigValue<Double> rareDropChance;
-		public final ConfigValue<Double> epicDropChance;
-		public final ConfigValue<Double> legendaryDropChance;
-		public final ConfigValue<Double> uncommonDropChancePerLuck;
-		public final ConfigValue<Double> rareDropChancePerLuck;
-		public final ConfigValue<Double> epicDropChancePerLuck;
-		public final ConfigValue<Double> legendaryDropChancePerLuck;
+		public final ConfigValue<Double> levelBonus;
+		public final ConfigValue<Boolean> showLevel;
 		
 		public Common(ForgeConfigSpec.Builder builder)
 		{
@@ -48,20 +35,6 @@ public class Config
 			armorBonus = builder.define("armor_per_level", 0.1D, positiveOrZeroDoublePredicate);
 			healthBonus = builder.define("health_per_level", 0.1D, positiveOrZeroDoublePredicate);
 			builder.pop(2);
-			builder.push("loot");
-			lootDropEnabled = builder.define("drop_enabled", true);
-			lootDropChance = builder.define("drop_chance", 0.2D, positiveOrZeroDoublePredicate);
-			lootDropChancePerLevel = builder.define("drop_chance_per_level", 0.05D, positiveOrZeroDoublePredicate);
-			lootDropChancePerLuck = builder.define("drop_chance_per_luck", 0.1D, positiveOrZeroDoublePredicate);
-			uncommonDropChance = builder.define("uncommon_loot_drop_chance", 0.75D, positiveOrZeroDoublePredicate);
-			rareDropChance = builder.define("rare_loot_drop_chance", 0.3D, positiveOrZeroDoublePredicate);
-			epicDropChance = builder.define("epic_loot_drop_chance", 0.05D, positiveOrZeroDoublePredicate);
-			legendaryDropChance = builder.define("legendary_loot_drop_chance", 0.01D, positiveOrZeroDoublePredicate);
-			uncommonDropChancePerLuck = builder.define("uncommon_loot_luck_bonus", 0.1D, positiveOrZeroDoublePredicate);
-			rareDropChancePerLuck = builder.define("rare_loot_luck_bonus", 0.02D, positiveOrZeroDoublePredicate);
-			epicDropChancePerLuck = builder.define("epic_loot_luck_bonus", 0.01D, positiveOrZeroDoublePredicate);
-			legendaryDropChancePerLuck = builder.define("legendary_loot_luck_bonus", 0.005D, positiveOrZeroDoublePredicate);
-			builder.pop(1);
 		}
 	}
 	
