ba452761bffb637ed223026bec7bd8beefbc6141
Daripher <saelver.serbek@gmail.com>
2022-11-22 18:18:43 +0500

    Renamed data package

--- src/main/java/daripher/autoleveling/command/CommandGlobalLevel.java
+++ src/main/java/daripher/autoleveling/command/CommandGlobalLevel.java
@@ -3,7 +3,7 @@ package daripher.autoleveling.command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 
-import daripher.autoleveling.data.GlobalLevelingData;
+import daripher.autoleveling.saveddata.GlobalLevelingData;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 
