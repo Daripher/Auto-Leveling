06ac5798eb485198a276011e49c2cec8f6ef6520
Daripher <saelver.serbek@gmail.com>
2022-11-27 23:08:16 +0500

    Removed AutoLevelingToolItem.class

--- src/main/java/daripher/autoleveling/item/WhitelistToolItem.java
+++ src/main/java/daripher/autoleveling/item/WhitelistToolItem.java
@@ -3,6 +3,7 @@ package daripher.autoleveling.item;
 import java.util.List;
 
 import daripher.autoleveling.config.Config;
+import daripher.autoleveling.init.AutoLevelingCreativeTabs;
 import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -10,13 +11,19 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class WhitelistToolItem extends AutoLevelingToolItem
+public class WhitelistToolItem extends Item
 {
+	public WhitelistToolItem()
+	{
+		super(new Properties().tab(AutoLevelingCreativeTabs.AUTO_LEVELING).stacksTo(1));
+	}
+	
 	@Override
 	public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity entity, InteractionHand hand)
 	{
