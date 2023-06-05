6fdb76da413f947788ee2a51aeb07ad5117ac53b
Daripher <saelver.serbek@gmail.com>
2023-06-05 20:37:38 +0500

    Fixed 0 commands permission level

---
+++ src/main/java/daripher/autoleveling/command/AutoLevelingCommands.java
@@ -0,0 +1,54 @@
+package daripher.autoleveling.command;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+
+import daripher.autoleveling.AutoLevelingMod;
+import daripher.autoleveling.saveddata.GlobalLevelingData;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(modid = AutoLevelingMod.MOD_ID)
+public class AutoLevelingCommands {
+	@SubscribeEvent
+	public static void onRegisterCommands(RegisterCommandsEvent event) {
+		var addGlobalLevelCommand = Commands.literal("autoleveling")
+				.then(Commands.literal("level")
+						.then(Commands.literal("add")
+								.then(Commands.argument("value", IntegerArgumentType.integer())
+										.executes(AutoLevelingCommands::executeAddLevelCommand))))
+				.requires(AutoLevelingCommands::hasPermission);
+		event.getDispatcher().register(addGlobalLevelCommand);
+		var getGlobalLevelCommand = Commands.literal("autoleveling")
+				.then(Commands.literal("level")
+						.then(Commands.literal("get")
+								.executes(AutoLevelingCommands::executeGetLevelCommand)))
+				.requires(AutoLevelingCommands::hasPermission);
+		event.getDispatcher().register(getGlobalLevelCommand);
+	}
+
+	private static int executeAddLevelCommand(CommandContext<CommandSourceStack> ctx) {
+		var server = ctx.getSource().getServer();
+		var globalLevelingData = GlobalLevelingData.get(server);
+		var levelBonus = ctx.getArgument("value", Integer.class);
+		var oldLevelBonus = globalLevelingData.getLevelBonus();
+		globalLevelingData.setLevel(oldLevelBonus + levelBonus);
+		return 1;
+	}
+
+	private static int executeGetLevelCommand(CommandContext<CommandSourceStack> ctx) {
+		var server = ctx.getSource().getServer();
+		var globalLevelingData = GlobalLevelingData.get(server);
+		var levelBonus = globalLevelingData.getLevelBonus();
+		ctx.getSource().sendSystemMessage(Component.literal("Global level bonus is " + levelBonus));
+		return 1;
+	}
+
+	private static boolean hasPermission(CommandSourceStack commandSourceStack) {
+		return commandSourceStack.hasPermission(2);
+	}
+}
