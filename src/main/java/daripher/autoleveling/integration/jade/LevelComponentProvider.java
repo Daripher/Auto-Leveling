b3c43de70c4636c26af0292e09728082437a47db
Daripher <saelver.serbek@gmail.com>
2023-06-08 20:55:28 +0500

    Fixes #9

--- src/main/java/daripher/autoleveling/integration/jade/LevelComponentProvider.java
+++ src/main/java/daripher/autoleveling/integration/jade/LevelComponentProvider.java
@@ -1,28 +1,35 @@
 package daripher.autoleveling.integration.jade;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import daripher.autoleveling.capability.LevelingDataProvider;
 import mcp.mobius.waila.api.EntityAccessor;
 import mcp.mobius.waila.api.IEntityComponentProvider;
 import mcp.mobius.waila.api.ITooltip;
 import mcp.mobius.waila.api.config.IPluginConfig;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 
 public enum LevelComponentProvider implements IEntityComponentProvider {
 	INSTANCE;
 
+	private static final Map<EntityType<?>, Boolean> SHOWN_LEVELS_CACHE = new HashMap<EntityType<?>, Boolean>();
+
+
 	@Override
 	public void appendTooltip(ITooltip tooltip, EntityAccessor entityAccessor, IPluginConfig pluginConfig) {
 		var entity = entityAccessor.getEntity();
-
-		if (!LevelingDataProvider.canHaveLevel(entity)) {
+		var entityType = entity.getType();
+		var shouldShowLevel = SHOWN_LEVELS_CACHE.get(entityType);
+		if (shouldShowLevel == null) {
+			shouldShowLevel = LevelingDataProvider.canHaveLevel(entity) && LevelingDataProvider.shouldShowLevel(entity);
+			SHOWN_LEVELS_CACHE.put(entityType, shouldShowLevel);
+		}
+		if (!shouldShowLevel) {
 			return;
 		}
-
-		if (!LevelingDataProvider.shouldShowLevel(entity)) {
-			return;
-		}
-
 		LevelingDataProvider.get((LivingEntity) entity).ifPresent(levelingData -> {
 			var entityLevel = levelingData.getLevel() + 1;
 			tooltip.add(new TranslatableComponent("jade.autoleveling.tooltip", entityLevel));
