eb6a879800d4b8dc1c1d8e0cfdbaca8bfc205c03
Daripher <saelver.serbek@gmail.com>
2023-11-20 14:52:44 +0500

    Fixed mob level being reset when summoned with commands

--- src/main/java/daripher/autoleveling/event/MobsLevelingEvents.java
+++ src/main/java/daripher/autoleveling/event/MobsLevelingEvents.java
@@ -67,7 +67,7 @@ public class MobsLevelingEvents {
   public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
     if (!shouldSetLevel(event.getEntity())) return;
     LivingEntity entity = (LivingEntity) event.getEntity();
-    if (entity.getTags().contains("autoleveling_spawned") && hasLevel(entity)) return;
+    if (entity.getTags().contains("autoleveling_spawned") || hasLevel(entity)) return;
     BlockPos spawnPos = getSpawnPosition(entity);
     double distanceFromSpawn = Math.sqrt(spawnPos.distSqr(entity.blockPosition()));
     int level = createLevelForEntity(entity, distanceFromSpawn);
